c0fcdbd8330a4ae6a9ba8c4615b44f6c56e2b3ac
Harshvardhan Kalra <harshvardhan14043@iiitd.ac.in>
2016-06-06 16:18:40 +0530

    Week 4 started

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '2038816963010903',
+        clientSecret: '30ad029f8c2bab5bea04afa89e1fe827',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
